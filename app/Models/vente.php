2ae367706ad19e2dbeb44f9c7fed20a0f2c0a289
ndiasse <ndiassefall29086@gmail.com>
2026-05-06 11:08:41 +0000

    commit sur branch

--- app/Models/vente.php
+++ app/Models/vente.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class vente extends Model
 {
+    use HasFactory;
     public function produit()
     {
         return $this->belongsTo(Produit::class, 'produit_id');
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends Factory<User>
@@ -25,11 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'prenom' => $this->faker->firstName(),
+            'nom' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'), // mot de passe par défaut
+            'role' => $this->faker->randomElement(['admin', 'employe']),
         ];
     }
 
@@ -38,7 +37,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
